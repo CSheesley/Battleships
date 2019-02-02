fc9c57933400e3cb16213830e496610e942315a1
Deonte Cooper <deonte.cooper12@gmail.com>
2019-02-01 18:18:01 -0700

    Frame work for runner class

---
+++ lib/runner.rb
@@ -0,0 +1,15 @@
+class Runner
+
+  def welcome_screen
+     "Welcome to BATTLESHIP \nEnter p to play. Enter q to quit."
+  end
+
+  def play_and_quit
+    gets.chomp "p"
+    if play
+    end
+
+
+
+  end
+end

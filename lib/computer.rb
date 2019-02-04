d48f7b2bad9baa64fff2aa61d3dc8430fa922f94
Corey Sheesley <corey.sheesley@gmail.com>
2019-02-04 08:34:02 -0700

    Refactored random coords methods

--- lib/computer.rb
+++ lib/computer.rb
@@ -8,28 +8,32 @@ class Computer
   end
 
   def place_ships
-    @board.place(@cruiser, generated_cells(@cruiser))
-    @board.place(@submarine, generated_cells(@submarine))
-    binding.pry
+    @board.place(@cruiser, generated_coords(@cruiser))
+    @board.place(@submarine, generated_coords(@submarine))
   end
 
-  def random_coordinates(cell_count)
-    all_coords = []
+  def random_coords(cell_count)
     selected_coords = []
-    all_coords << board.cells.keys
-    all_coords.flatten!
     selected_coords << all_coords.sample(cell_count)
     selected_coords.flatten!
   end
 
-  def generated_cells(ship)
+  def all_coords
+    all_coords = []
+    all_coords << board.cells.keys
+    all_coords.flatten!
+  end
+
+  def generated_coords(ship)
     loop do
-      @coordinates = random_coordinates(ship.length)
+      @coordinates = random_coords(ship.length)
       if @board.valid_placement?(ship, @coordinates )
         break
       end
     end
     return @coordinates
   end
+
+
 
 end
